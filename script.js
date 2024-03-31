91beb2b899b34d4eac9fa1401f413587534f21da
Cristian da Rosa <cristian.rosa@outlook.com>
2024-03-31 04:30:00 -0300

    finalizei quase todos html

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Redirecionando para as páginas quando clico em alguns botões
 function redireciona(page) {
     window.location.href = page
+}
+
+// Colocando foco automático no campo nome do cadastro do funcionário
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('nome').focus();
+});
+
+function excluiHorario() {
+    
 }
